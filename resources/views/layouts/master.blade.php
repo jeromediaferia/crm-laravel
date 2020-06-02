465224742a98cf0240f9cbf80ade207cb7723b40
Jérôme Diaferia <pro.jerome.diaferia@gmail.com>
2020-06-02 12:30:42 +0200

    Gestion des CSS et JS

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, in!</p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
